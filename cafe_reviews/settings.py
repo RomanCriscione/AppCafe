ec5840da404e242417f22dc073e77da76c8c02fc
RomanCriscione <criscioner@gmail.com>
2025-07-29 20:28:26 -0300

    BackUp 29-7

--- cafe_reviews/settings.py
+++ cafe_reviews/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +64,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cafe_reviews.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
