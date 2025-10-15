4036b51f64ca6b61df88a7c598dfbdfbcee24175
RomanCriscione <criscioner@gmail.com>
2025-10-15 08:14:28 -0300

    Back Up 15-10

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,12 @@
-// JS global mínimo
-document.addEventListener('DOMContentLoaded', () => {});
+// static/js/main.js
+document.addEventListener('DOMContentLoaded', () => {
+  // Helper CSRF para fetch POST
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+  window.csrftoken = getCookie('csrftoken') || '';
+
+
+});
