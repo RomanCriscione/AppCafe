85fd0dddb394a9953f1c6cadba08711790c0931b
RomanCriscione <criscioner@gmail.com>
2026-01-06 18:36:05 -0300

    BackUp 6-1

--- cafe_reviews/urls.py
+++ cafe_reviews/urls.py
@@ -1,67 +1,82 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
-from django.urls import static
-from django.views.generic import TemplateView, RedirectView
-from django.contrib.sitemaps.views import sitemap
+from django.views.generic import RedirectView, TemplateView
 
+from django.contrib.sitemaps.views import sitemap
 from cafe_reviews.sitemaps import sitemaps
+
 from rest_framework.routers import DefaultRouter
 from reviews.api import CafeViewSet
 
+# Router API
 router = DefaultRouter()
 router.register(r"cafes", CafeViewSet, basename="cafe")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # Allauth
+    # Autenticación Allauth
     path("accounts/", include("allauth.urls")),
 
     # Rutas de usuarios
     path("users/", include("accounts.urls")),
 
-    # Páginas públicas
+    # Público – páginas estáticas
     path("", include("core.urls")),
 
-    # HTML de cafés
+    # Cafés HTML
     path("reviews/", include("reviews.urls")),
 
-    # API
+    # API REST
     path("api/", include(router.urls)),
 
-    # robots
+    # robots.txt
     path(
         "robots.txt",
         TemplateView.as_view(
             template_name="core/robots.txt",
-            content_type="text/plain"
+            content_type="text/plain",
         ),
         name="robots_txt",
     ),
 
     # sitemap
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django_sitemap"),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django_sitemap",
+    ),
 
     # favicon
     path(
         "favicon.ico",
-        RedirectView.as_view(url="/static/images/coffee-icon.png"),
+        RedirectView.as_view(
+            url="/static/images/coffee-icon.png",
+            permanent=False,
+        ),
         name="favicon",
     ),
 
-    # --- Google verification ---
+    # --- VERIFICACIÓN GOOGLE – SERVIR COMO ESTÁTICO ---
     path(
         "google157d26ef7e3007f2.html",
-        static.serve,
-        {"document_root": settings.STATIC_ROOT},
+        RedirectView.as_view(
+            url="/static/google157d26ef7e3007f2.html",
+            permanent=False,
+        ),
         name="google_verify",
     ),
 ]
 
-# MEDIA fallback
+# MEDIA en Render
 urlpatterns += [
-    re_path(r"^media/(?P<path>.*)$", settings.MEDIA_ROOT),
+    re_path(
+        r"^media/(?P<path>.*)$",
+        django.views.static.serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
 ]
 
 if settings.DEBUG:
