49bf63e547fe17bb96a90828f89f154f562ab0fa
RomanCriscione <criscioner@gmail.com>
2026-01-06 17:01:15 -0300

    BackUp 6-1

--- cafe_reviews/urls.py
+++ cafe_reviews/urls.py
@@ -1,14 +1,11 @@
-# cafe_reviews/urls.py
 from django.contrib import admin
-from django.urls import path, include, re_path      # ⬅️ re_path
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView, RedirectView
-from django.contrib.sitemaps.views import sitemap
-from django.views.static import serve as media_serve # ⬅️ media_serve
-from cafe_reviews.sitemaps import sitemaps
 
-from django.templatetags.static import static as static_url
+from django.contrib.sitemaps.views import sitemap
+from cafe_reviews.sitemaps import sitemaps   # usa el registro que ya corregimos
+
 from rest_framework.routers import DefaultRouter
 from reviews.api import CafeViewSet
 
@@ -24,49 +21,53 @@ urlpatterns = [
     # Users
     path("users/", include("accounts.urls")),
 
-    # Público
+    # Público – páginas estáticas y home
     path("", include("core.urls")),
 
     # Cafés (HTML)
-    path("reviews/", include(("reviews.urls", "reviews"), namespace="reviews")),
+    path("reviews/", include("reviews.urls")),   # el namespace ya viene del archivo reviews/urls.py
 
-    # API
+    # API REST
     path("api/", include(router.urls)),
 
     # robots.txt
     path(
         "robots.txt",
-        TemplateView.as_view(template_name="core/robots.txt", content_type="text/plain"),
+        TemplateView.as_view(
+            template_name="core/robots.txt",
+            content_type="text/plain"
+        ),
         name="robots_txt",
     ),
 
     # sitemap
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django_sitemap"),
 
-    # favicon (archivo real en /static/images/)
+    # favicon
     path(
         "favicon.ico",
-        RedirectView.as_view(url=static_url("images/coffee-icon.png"), permanent=False),
+        RedirectView.as_view(
+            url="/static/images/coffee-icon.png",
+            permanent=False
+        ),
         name="favicon",
     ),
 
-    # --- Verificación Google ---
+    # --- Verificación Google (archivo en raíz) ---
     path(
         "google157d26ef7e3007f2.html",
-        media_serve,
-        {"document_root": settings.STATIC_ROOT},
+        TemplateView.as_view(
+            template_name="google157d26ef7e3007f2.html",
+            content_type="text/plain"
+        ),
         name="google_verify",
     ),
-
 ]
 
-# ✅ Servir MEDIA también en producción (Render)
+# Servir MEDIA en producción
 urlpatterns += [
-    re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
-
-    
+    re_path(r"^media/(?P<path>.*)$", RedirectView.as_view(url="/static/")),
 ]
 
-# (Opcional) si querés mantener la variante clásica para dev:
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
