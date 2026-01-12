f258c2b49694a0d49cbe324ad0f7a2d6b642262b
RomanCriscione <criscioner@gmail.com>
2026-01-12 19:37:55 -0300

    BackUp 12-1

--- cafe_reviews/settings.py
+++ cafe_reviews/settings.py
@@ -123,6 +123,21 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = "optional"
 
 # ======================================================
+# EMAIL
+# ======================================================
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = "Gota <gotacafe.test@gmail.com>"
+
+
+# ======================================================
 # PASSWORD VALIDATION
 # ======================================================
 AUTH_PASSWORD_VALIDATORS = [
