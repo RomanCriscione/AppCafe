d0b9e8be3e35d9fe08fcff7881d5d5fd436eebf7
RomanCriscione <criscioner@gmail.com>
2026-02-15 10:54:31 -0300

    V1.1.1

--- cafe_reviews/settings.py
+++ cafe_reviews/settings.py
@@ -195,3 +195,19 @@ LOGGING = {
         },
     },
 }
+
+# ======================================================
+# CLOUDINARY (IMÁGENES)
+# ======================================================
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
