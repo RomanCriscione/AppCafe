c8e5b0aff0542a24883737d65d695a689d195d78
RomanCriscione <criscioner@gmail.com>
2026-05-15 08:38:51 -0300

    V.1.2.5

--- cafe_reviews/settings.py
+++ cafe_reviews/settings.py
@@ -134,8 +134,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 
 DEFAULT_FROM_EMAIL = "Gota <no_reply@gogota.ar>"
 
