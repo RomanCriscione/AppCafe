29c8d75ce79e8fb6992629f6dcdb67fb9a631adf
RomanCriscione <criscioner@gmail.com>
2026-06-04 14:43:33 -0300

    V.1.2.16

--- reviews/models.py
+++ reviews/models.py
@@ -33,7 +33,41 @@ class Cafe(models.Model):
     name = models.CharField(max_length=100, unique=True)
     address = models.CharField(max_length=255)
     location = models.CharField(max_length=100)
-    province = models.CharField(max_length=100,default="Buenos Aires",verbose_name="Provincia")
+    PROVINCE_CHOICES = [
+        ("", "Seleccioná una provincia"),
+        ("CABA", "Ciudad Autónoma de Buenos Aires"),
+        ("Buenos Aires", "Buenos Aires"),
+        ("Catamarca", "Catamarca"),
+        ("Chaco", "Chaco"),
+        ("Chubut", "Chubut"),
+        ("Córdoba", "Córdoba"),
+        ("Corrientes", "Corrientes"),
+        ("Entre Ríos", "Entre Ríos"),
+        ("Formosa", "Formosa"),
+        ("Jujuy", "Jujuy"),
+        ("La Pampa", "La Pampa"),
+        ("La Rioja", "La Rioja"),
+        ("Mendoza", "Mendoza"),
+        ("Misiones", "Misiones"),
+        ("Neuquén", "Neuquén"),
+        ("Río Negro", "Río Negro"),
+        ("Salta", "Salta"),
+        ("San Juan", "San Juan"),
+        ("San Luis", "San Luis"),
+        ("Santa Cruz", "Santa Cruz"),
+        ("Santa Fe", "Santa Fe"),
+        ("Santiago del Estero", "Santiago del Estero"),
+        ("Tierra del Fuego", "Tierra del Fuego"),
+        ("Tucumán", "Tucumán"),
+    ]
+
+    province = models.CharField(
+        max_length=100,
+        choices=PROVINCE_CHOICES,
+        blank=True,
+        default="",
+        verbose_name="Provincia"
+    )
     description = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     google_maps_url = models.URLField(blank=True, null=True)
