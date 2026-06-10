f19f1b122c4f8733dd05b38d9988abe40f20ed8c
RomanCriscione <criscioner@gmail.com>
2026-06-10 20:36:24 -0300

    V.1.2.99

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,18 +1,33 @@
-# reviews/serializers.py
 from rest_framework import serializers
 from reviews.models import Cafe, Tag
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ["id", "name", "category"]
 
+
 class CafeSerializer(serializers.ModelSerializer):
     # Este campo viene anotado en la queryset (api.py)
     average_rating = serializers.FloatField(read_only=True)
 
-    # Si querés devolver los nombres de tags:
+    # Tags
     tags = TagSerializer(many=True, read_only=True)
+
+    # Foto principal para Mobile
+    photo1_url = serializers.SerializerMethodField()
+
+    def get_photo1_url(self, obj):
+        if obj.photo1:
+            request = self.context.get("request")
+
+            if request:
+                return request.build_absolute_uri(obj.photo1.url)
+
+            return obj.photo1.url
+
+        return None
 
     class Meta:
         model = Cafe
@@ -27,5 +42,6 @@ class CafeSerializer(serializers.ModelSerializer):
             "is_pet_friendly",
             "is_vegan_friendly",
             "average_rating",
+            "photo1_url",
             "tags",
         ]
