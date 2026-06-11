95ced15e5c16c64f31b11aefeac0f056651c335b
RomanCriscione <criscioner@gmail.com>
2026-06-10 22:01:57 -0300

    V.1.2.100

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from rest_framework import serializers
 from reviews.models import Cafe, Tag
+
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -17,6 +19,7 @@ class CafeSerializer(serializers.ModelSerializer):
 
     # Foto principal para Mobile
     photo1_url = serializers.SerializerMethodField()
+    top_tags = serializers.SerializerMethodField()
 
     def get_photo1_url(self, obj):
         if obj.photo1:
@@ -28,6 +31,15 @@ class CafeSerializer(serializers.ModelSerializer):
             return obj.photo1.url
 
         return None
+    
+    def get_top_tags(self, obj):
+        return list(
+            Tag.objects
+            .filter(reviews__cafe=obj)
+            .annotate(num=Count("id"))
+            .order_by("-num", "name")
+            .values_list("name", flat=True)[:5]
+        )
 
     class Meta:
         model = Cafe
@@ -43,5 +55,6 @@ class CafeSerializer(serializers.ModelSerializer):
             "is_vegan_friendly",
             "average_rating",
             "photo1_url",
+            "top_tags",
             "tags",
         ]
